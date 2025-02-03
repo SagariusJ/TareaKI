d533c7c651eed20363d8f316afe14f8b5bd65e01
Manuel Mendoza <mmendoza@NTB-219.local>
2025-02-03 14:05:17 -0300

    Testing APIs ejecute: CRUD-Cookies,CRUD-Politics-Cookies

--- test/cypress/e2e/APIs/BD-Swagger.cy.js
+++ test/cypress/e2e/APIs/BD-Swagger.cy.js
@@ -25,30 +25,25 @@ describe('API Testing', () => {
     cy.get('input').type('21');
     cy.get('.execute-wrapper > .btn').click();
     cy.get(':nth-child(1) > .responses-table > tbody > .response > .response-col_status').should('have.text', '200');
-    cy.get('.response-col_description > :nth-child(1) > .highlight-code > .microlight > .language-json > :nth-child(10)').should('include.text', 'Testing Cookies 2025');
+    cy.get('.response-col_description > :nth-child(1) > .highlight-code > .microlight > .language-json > :nth-child(10)').should('include.text', 'Testing Cookies update - 2025');
     })
     it('PUT /api/Cookies/{id}', () => {
-    cy.get('#operations-Cookies-put_api_Cookies__id_ > .opblock-summary > .opblock-control-arrow').click();
-    cy.get('.btn').click();
-    cy.get('input').type('21');
-    //update testing id 21
-    cy.get('.body-param__text').clear().type('{"id": 21,"name": "Testing Cookies Update","description": "But I must explain to you how all ....."}', { parseSpecialCharSequences: false });
-    cy.get('.execute-wrapper > .btn').click();
-    cy.get('#put_api_Cookies__id__responses > tbody > .response > .response-col_status').should('have.text', '200');
-    //Validate update
-    cy.get('#operations-Cookies-put_api_Cookies__id_ > .opblock-summary > .opblock-control-arrow').click();
-    cy.get('#operations-Cookies-get_api_Cookies__id_ > .opblock-summary > .opblock-control-arrow').click();
-    cy.get('.btn').click();
-    cy.get('input').type('21');
-    cy.get('.execute-wrapper > .btn').click();
-    cy.get('.response-col_description > :nth-child(1) > .highlight-code > .microlight > .language-json > :nth-child(15)').should('include.text', '"But test........"');
-    cy.get('#get_api_Cookies__id__responses > tbody > .response > .response-col_status').should('have.text', '200');
-    cy.get('#operations-Cookies-get_api_Cookies__id_ > .opblock-summary > .opblock-control-arrow').click();
-    // restablecer a su origen
-    cy.get('#operations-Cookies-put_api_Cookies__id_ > .opblock-summary > .opblock-control-arrow').click();
-    cy.get('.reset').click();
-    cy.get('.body-param__text').clear().type('{"id": 21,"name": "Testing Cookies","description": "But I must explain to you how all ....."}', { parseSpecialCharSequences: false });
-    cy.get('.execute').click();
+      cy.get('#operations-Cookies-put_api_Cookies__id_ > .opblock-summary > .opblock-control-arrow').click();
+      cy.get('.btn').click();
+      cy.get('input').type('21');
+      //update testing id 21
+      cy.get('.body-param__text').clear().type('{"accepted": true}', { parseSpecialCharSequences: false });
+      cy.get('.execute-wrapper > .btn').click();
+      cy.get('#put_api_Cookies__id__responses > tbody > .response > .response-col_status').should('have.text', '200');
+      //Validate update
+      cy.get('#operations-Cookies-put_api_Cookies__id_ > .opblock-summary > .opblock-control-arrow').click();
+      cy.get('#operations-Cookies-get_api_Cookies__id_ > .opblock-summary > .opblock-control-arrow').click();
+      cy.get('.btn').click();
+      cy.get('input').type('21');
+      cy.get('.execute-wrapper > .btn').click();
+      cy.get('.example > .language-json > :nth-child(25)').should('include.text', 'true');
+      cy.get('#get_api_Cookies__id__responses > tbody > .response > .response-col_status').should('have.text', '200');
+      cy.get('#operations-Cookies-get_api_Cookies__id_ > .opblock-summary > .opblock-control-arrow').click();
     })
     it('DELETE /api/Cookies/{id}', () => {
     // create test
@@ -73,85 +68,78 @@ describe('API Testing', () => {
    
     
     })
-    it.only('PUT /api/Cookies/update-status{id}', () => {
-      cy.get('#operations-Cookies-put_api_Cookies_update_status__id_ > .opblock-summary > .opblock-control-arrow').click();    cy.get('.btn').click();
-    //   cy.get('.execute').click();
-    //   cy.get(':nth-child(1) > .responses-table > tbody > .response > .response-col_status').should('have.text', '200');
+    it('PUT /api/Cookies/update-status-full{id}', () => {
+      cy.get('#operations-Cookies-put_api_Cookies_update_cookie_full__id_ > .opblock-summary > .opblock-control-arrow').click();    
+      cy.get('.btn').click();
+      cy.get('input').type('21');
+
+      cy.get('.body-param__text').clear().type('{"id": 21,"name": "Testing Cookies update - 2025","description": "But test update........","required":false,"accepted":false}', { parseSpecialCharSequences: false });
+
+      cy.get('.execute').click();
+      cy.get('#put_api_Cookies_update-cookie-full__id__responses > tbody > .response > .response-col_status').should('have.text', '200');
     })
   });
   describe('CRUD-Politicas Cookies', () => {
-    it('GET /api/Cookies', () => {
-    cy.get('#operations-Cookies-get_api_Cookies > .opblock-summary > .opblock-control-arrow').click();
-    cy.get('.btn').click(); 
-    cy.get('.execute-wrapper > .btn').click();
-    cy.get(':nth-child(1) > .responses-table > tbody > .response > .response-col_status').should('have.text', '200')
+    it('GET/api/Politics', () => {
+      cy.get('#operations-Politics-get_api_Politics > .opblock-summary > .opblock-control-arrow').click();
+      cy.get('.btn').click(); 
+      cy.get('.execute-wrapper > .btn').click();
+      cy.get(':nth-child(1) > .responses-table > tbody > .response > .response-col_status').should('have.text', '200')
     })
-    it('POST /api/Cookies/{id}', () => {
-    cy.get('#operations-Cookies-post_api_Cookies > .opblock-summary > .opblock-control-arrow').click();
-    cy.get('.btn').click();
-    cy.get('.body-param__text').clear().type('{"id": 0,"name": "Testing Cookies 2025","description": "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"}', { parseSpecialCharSequences: false });
-    cy.get('.execute-wrapper > .btn').click();
-    cy.get('.response-col_status').should('include.text', '201');
+    it('POST /api/Politics{id}', () => {
+      cy.get('#operations-Politics-post_api_Politics > .opblock-summary > .opblock-control-arrow').click();
+      cy.get('.btn').click();
+      cy.get('.body-param__text').clear().type('{"id": 0,"title": "Politics Cookies 2025","description": "But I must explain....."}', { parseSpecialCharSequences: false });
+      cy.get('.execute-wrapper > .btn').click();
+      cy.get('.response-col_status').should('include.text', '201');
     })
-    it('GET /api/Cookies/{id}', () => {
-    cy.get('#operations-Cookies-get_api_Cookies__id_ > .opblock-summary > .opblock-control-arrow').click();
-    cy.get('.btn').click();
-    cy.get('input').type('32');
-    cy.get('.execute-wrapper > .btn').click();
-    cy.get(':nth-child(1) > .responses-table > tbody > .response > .response-col_status').should('have.text', '200');
-    cy.get('.response-col_description > :nth-child(1) > .highlight-code > .microlight > .language-json > :nth-child(10)').should('include.text', 'Testing Cookies');
+    it('GET /api/Politics/{id}', () => {
+      cy.get('#operations-Politics-get_api_Politics__id_ > .opblock-summary > .opblock-control-arrow').click();
+      cy.get('.btn').click();
+      cy.get('input').type('5');
+      cy.get('.execute-wrapper > .btn').click();
+      cy.get(':nth-child(1) > .responses-table > tbody > .response > .response-col_status').should('have.text', '200');
+      cy.get('.response-col_description > :nth-child(1) > .highlight-code > .microlight').should('include.text', 'Politics Cookies 2025');
     })
-    it('PUT /api/Cookies/{id}', () => {
-    cy.get('#operations-Cookies-put_api_Cookies__id_ > .opblock-summary > .opblock-control-arrow').click();
-    cy.get('.btn').click();
-    cy.get('input').type('32');
-    //update testing id 32
-    cy.get('.body-param__text').clear().type('{"id": 21,"name": "Testing Cookies Update","description": "But I must explain to you how all ....."}', { parseSpecialCharSequences: false });
-    cy.get('.execute-wrapper > .btn').click();
-    cy.get('#put_api_Cookies__id__responses > tbody > .response > .response-col_status').should('have.text', '200');
-    //Validate update
-    cy.get('#operations-Cookies-put_api_Cookies__id_ > .opblock-summary > .opblock-control-arrow').click();
-    cy.get('#operations-Cookies-get_api_Cookies__id_ > .opblock-summary > .opblock-control-arrow').click();
-    cy.get('.btn').click();
-    cy.get('input').type('32');
-    cy.get('.execute-wrapper > .btn').click();
-    cy.get('.response-col_description > :nth-child(1) > .highlight-code > .microlight > .language-json > :nth-child(15)').should('include.text', '"But I must explain to you how all ....."');
-    cy.get('#get_api_Cookies__id__responses > tbody > .response > .response-col_status').should('have.text', '200');
-    cy.get('#operations-Cookies-get_api_Cookies__id_ > .opblock-summary > .opblock-control-arrow').click();
-    // restablecer a su origen
-    cy.get('#operations-Cookies-put_api_Cookies__id_ > .opblock-summary > .opblock-control-arrow').click();
-    cy.get('.reset').click();
-    cy.get('.body-param__text').clear().type('{"id": 21,"name": "Testing Cookies","description": "But I must explain to you how all ....."}', { parseSpecialCharSequences: false });
-    cy.get('.execute').click();
+    it('PUT /api/Politics/{id}', () => {
+      cy.get('#operations-Politics-put_api_Politics__id_ > .opblock-summary > .opblock-control-arrow').click();
+      cy.get('.btn').click();
+      cy.get('input').type('6');
+      //update testing id 6
+      cy.get('.body-param__text').clear().type('{"id": 6,"title": "Testing Politics Cookies Update","description": "But I must explain to you how all UPDATE....."}', { parseSpecialCharSequences: false });
+      cy.get('.execute-wrapper > .btn').click();
+      cy.get('#put_api_Politics__id__responses > tbody > .response > .response-col_status').should('have.text', '200');
+      //Validate update
+      cy.get('#operations-Politics-put_api_Politics__id_ > .opblock-summary > .opblock-control-arrow').click();
+      cy.get('#operations-Politics-get_api_Politics__id_ > .opblock-summary > .opblock-control-arrow').click();
+      cy.get('.btn').click();
+      cy.get('input').type('6');
+      cy.get('.execute-wrapper > .btn').click();
+      cy.get('.response-col_description > :nth-child(1) > .highlight-code > .microlight').should('include.text', '"But I must explain to you how all UPDATE....."');
+      cy.get('#get_api_Politics__id__responses > tbody > .response > .response-col_status').should('have.text', '200');
+  
     })
-    it('DELETE /api/Cookies/{id}', () => {
+    it('DELETE /api/Politics/{id}', () => {
     // create test
-    cy.get('#operations-Cookies-post_api_Cookies > .opblock-summary > .opblock-control-arrow').click();
-    cy.get('.btn').click();
-    cy.get('.body-param__text').clear().type('{"id": 0,"name": "Testing Delete Cookies","description": "Testing Delete ......"}', { parseSpecialCharSequences: false });
-    cy.get('.execute-wrapper > .btn').click();
-    cy.get('.response-col_description > :nth-child(1) > .highlight-code > .microlight > .language-json > [style="color: rgb(211, 99, 99);"]').invoke('text').then((text)=>{
-      cy.get('#operations-Cookies-post_api_Cookies > .opblock-summary > .opblock-control-arrow').click();
-      cy.get('#operations-Cookies-delete_api_Cookies__id_ > .opblock-summary > .opblock-control-arrow').click();
+    cy.get('#operations-Politics-post_api_Politics > .opblock-summary > .opblock-control-arrow').click();
       cy.get('.btn').click();
-      cy.get('input').type(text);
+      cy.get('.body-param__text').clear().type('{"id": 0,"title": "Testing Delete Politics Cookies","description": "Testing Delete ......"}', { parseSpecialCharSequences: false });
       cy.get('.execute-wrapper > .btn').click();
-      //validar testing eliminado
-      cy.get('#operations-Cookies-delete_api_Cookies__id_ > .opblock-summary > .opblock-control-arrow').click();
-      cy.get('#operations-Cookies-get_api_Cookies__id_ > .opblock-summary > .opblock-control-arrow').click();
-      cy.get('.btn').click();
-      cy.get('input').type(text);
-      cy.get('.execute-wrapper > .btn').click();
-      cy.get(':nth-child(1) > .responses-table > tbody > .response > .response-col_status').should('include.text', '404');
-    });
-   
+      cy.get('.response-col_description > :nth-child(1) > .highlight-code > .microlight > .language-json > [style="color: rgb(211, 99, 99);"]').invoke('text').then((text)=>{
+        cy.get('#operations-Politics-post_api_Politics > .opblock-summary > .opblock-control-arrow').click();
+        cy.get('#operations-Politics-delete_api_Politics__id_ > .opblock-summary > .opblock-control-arrow').click();
+        cy.get('.btn').click();
+        cy.get('input').type(text);
+        cy.get('.execute-wrapper > .btn').click();
+        //validar testing eliminado
+        cy.get('#operations-Politics-delete_api_Politics__id_ > .opblock-summary > .opblock-control-arrow').click();
+        cy.get('#operations-Politics-get_api_Politics__id_ > .opblock-summary > .opblock-control-arrow').click();
+        cy.get('.btn').click();
+        cy.get('input').type(text);
+        cy.get('.execute-wrapper > .btn').click();
+        cy.get(':nth-child(1) > .responses-table > tbody > .response > .response-col_status').should('include.text', '404');
+      });
+    })
     
-    })
-    it('GET /WeatherForecast', () => {
-    cy.get('#operations-WeatherForecast-GetWeatherForecast > .opblock-summary > .opblock-control-arrow').click();
-    cy.get('.btn').click();
-    cy.get('.execute').click();
-    cy.get(':nth-child(1) > .responses-table > tbody > .response > .response-col_status').should('have.text', '200');
-    })
   });
 })
